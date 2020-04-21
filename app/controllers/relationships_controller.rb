79521ae0724d8c0d5d4e386773a5435dc642fe17
sora-uzu <naruto07170717@gmail.com>
2020-04-21 17:06:17 +0000

    [Add]Ajax to follow_btn

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,13 @@
 class RelationshipsController < ApplicationController
 
  def create
-	user = User.find(params[:following_id])
-	current_user.follow(user)
-	redirect_back(fallback_location: root_path)
+	@user = User.find(params[:following_id])
+	current_user.follow(@user)
  end
 
  def destroy
-	user = Relationship.find(params[:id]).following
-	current_user.unfollow(user)
-	redirect_back(fallback_location: root_path)
+	@user = Relationship.find(params[:id]).following
+	current_user.unfollow(@user)
  end
 
 end
