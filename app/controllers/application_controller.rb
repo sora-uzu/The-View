7ffcc06d4e7a357b19dafe326a2bf7defc61156b
sora-uzu <naruto07170717@gmail.com>
2020-05-05 18:24:24 +0900

    [Fix]rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(_resource)
